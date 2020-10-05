29bc0c29e048082678f08730e5669402ac3e797a
ionpetro <ionpetro@gmail.com>
2020-10-05 18:39:17 +0300

    remove dead code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,15 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit{
   title:string = 'angular-academy';
-  access:string = 'NO';
 
   ngOnInit() {
-    sessionStorage.setItem('access', this.access);
   }
 
   setSessionStorage() {
-    this.access = 'YES';
-    sessionStorage.setItem('access', this.access);
   }
   
 }
